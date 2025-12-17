4fdca85ce9c1401dedd6105df9ac2aa1d8ba7ffc
RACHELWITNER <rachel.witner@dpi.nc.gov>
2025-12-17 16:08:50 +0000

    remove folder

--- tasty_bytes_dbt_demo/deployment.sql
+++
@@ -1,6 +0,0 @@
-create or replace dbt project TASTY_BYTES_DBT_DB.PROD.EXAMPLE_DBT_PROJECT
-	-- from snow://workspace/USER$RACHELWITNER.PUBLIC."tasty_bytes_dbt"/versions/live/tasty_bytes_dbt_demo/
-    FROM '@tasty_bytes_dbt_db.prod.dbt_production_repo/branches/main/tasty_bytes_dbt_demofolder/'
-	DEFAULT_TARGET='prod'
-    -- external_access_integrations = ;
-    ;
